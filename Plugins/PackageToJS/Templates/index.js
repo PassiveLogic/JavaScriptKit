3d1907da3817a85ca3a11cca559da93fab876548
Krzysztof Rodak <krodak.konta@gmail.com>
2026-04-17 12:45:17 +0200

    fix: Exclude identityMode from DefaultNodeSetupOptions spread in index.js
    TypeScript strict excess property check rejects identityMode in the spread
    into DefaultNodeSetupOptions. Destructure it out before spreading, since
    it's already handled separately via the instantiateOptions pass-through.

--- Plugins/PackageToJS/Templates/index.js
+++ Plugins/PackageToJS/Templates/index.js
@@ -9,9 +9,10 @@ import { defaultBrowserSetup /* #if USE_SHARED_MEMORY */, createDefaultWorkerFac
 /* #if TARGET_DEFAULT_PLATFORM_NODE */
 /** @type {import('./index.d').init} */
 async function initNode(_options) {
+    const { identityMode: _identityMode, ...rest } = _options || {};
     /** @type {import('./platforms/node.d.ts').DefaultNodeSetupOptions} */
     const options = {
-        ...(_options || {}),
+        ...rest,
 /* #if USE_SHARED_MEMORY */
         spawnWorker: createDefaultWorkerFactoryForNode(),
 /* #endif */
